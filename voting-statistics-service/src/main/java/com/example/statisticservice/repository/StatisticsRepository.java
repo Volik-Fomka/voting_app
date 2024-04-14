00a2ae268e39cf4dcf0656b3fbbdf62f678ee514
Artyom Volozhanin <artemiefermer@gmail.com>
2024-04-14 22:20:39 +0300

    Улучшен и доработан сервис для показа статистики

--- voting-statistics-service/src/main/java/com/example/statisticservice/repository/StatisticsRepository.java
+++ voting-statistics-service/src/main/java/com/example/statisticservice/repository/StatisticsRepository.java
@@ -1,15 +1,14 @@
 package com.example.statisticservice.repository;
 
 import com.example.statisticservice.model.Vote;
+import com.example.statisticservice.model.VoteStatistics;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface StatisticsRepository extends JpaRepository<Vote, Long> {
-    @Query("SELECT COUNT(v) FROM Vote v WHERE v.selection = 'Cat'")
-    long countCats();
-
-    @Query("SELECT COUNT(v) FROM Vote v WHERE v.selection = 'Dog'")
-    long countDogs();
+    @Query("SELECT new com.example.statisticservice.model.VoteStatistics(SUM(CASE WHEN v.selection = 'Cat' THEN 1 ELSE 0 END), SUM(CASE WHEN v.selection = 'Dog' THEN 1 ELSE 0 END)) FROM Vote v")
+    Optional<VoteStatistics> countVotes(); 
 }
